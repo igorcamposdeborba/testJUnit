88f1a8edaa3ed200c999adb2a9846ea8da5703e5
igorborba <igor_campos5@hotmail.com>
2023-01-18 21:18:01 -0300

    Test entire class with JUnit

--- 2TestEntireClass/src/tests/entities/FinancingTests.java
+++ 2TestEntireClass/src/tests/entities/FinancingTests.java
@@ -36,6 +36,23 @@ public class FinancingTests {
 		}, "A parcela nao pode ser maior que a metade da renda");
 		
 		Assertions.assertEquals("A parcela nao pode ser maior que a metade da renda", error.getMessage());
+	}
+	
+	@Test
+	public void setTotalAmountShouldUpdateAmountWhenValidData() {
+		// 1) ARRANGE
+		Double totalAmount = 100000.0;
+		Double income = 2000.0;
+		Integer months = 80;
+		Financing installments = FinancingFactory.createFinancing(totalAmount, income, months);
+		
+		Double expectedTotalAmount = 50000.0;
+		
+		// 2) ACT
+		installments.setTotalAmount(expectedTotalAmount);
+		
+		// 3) ASSERT
+		Assertions.assertEquals(expectedTotalAmount, installments.getTotalAmount());
 		
 	}
 }
