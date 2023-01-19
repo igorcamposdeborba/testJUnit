0b32985bc7c7560fe316e0f760c887631199611b
igorborba <igor_campos5@hotmail.com>
2023-01-18 21:06:00 -0300

    Test entire class with JUnit

---
+++ 2TestEntireClass/src/tests/entities/FinancingTests.java
@@ -0,0 +1,41 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
+import tests.factory.FinancingFactory;
+
+public class FinancingTests {
+	// Padrão de nomeclatura de TESTE: <AÇÃO> Should <EFEITO> [When Cenário]
+	@Test
+	public void constructorShouldCreateFinancingObjectWhenValidInputs() {
+		// 1) ARRANGE
+		Double totalAmount = 100000.0;
+		Double income = 2000.0;
+		Integer months = 80;
+				
+		// 2) ACT
+		Financing installments = FinancingFactory.createFinancing(totalAmount, income, months);
+		
+		// 3) ASSERT
+		Assertions.assertNotNull(installments);
+	}
+	
+	@Test
+	public void constructorShoulThrowExceptionWhenInvalidInputs() {
+		// 1) ARRANGE
+		Double totalAmount = 100000.0;
+		Double income = 2000.0;
+		Integer months = 20;
+		
+		
+		// 2) ACT and ASSERT
+		IllegalArgumentException error = Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> {
+			FinancingFactory.createFinancing(totalAmount, income, months);
+		}, "A parcela nao pode ser maior que a metade da renda");
+		
+		Assertions.assertEquals("A parcela nao pode ser maior que a metade da renda", error.getMessage());
+		
+	}
+}
