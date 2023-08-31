1769328396184e2a935043651a1559e0e12779b3
igorcamposdeborba <igor_campos5@hotmail.com>
2023-08-31 20:11:51 -0300

    Validar vários campos com a mesma exception, Lista de exceptions, AssertAll com AssertThrows

--- gerenciadorFinanceiro/gerenciadorFinanceiro/src/test/java/br/com/iborba/gerenciadorFinanceiro/domain/UserTest.java
+++ gerenciadorFinanceiro/gerenciadorFinanceiro/src/test/java/br/com/iborba/gerenciadorFinanceiro/domain/UserTest.java
@@ -10,6 +10,7 @@ import br.com.iborba.gerenciadorFinanceiro.domain.exceptions.ValidationException
 
 class UserTest {
 
+	// ! Como pegar uma lista de Exceptions ao invés de descobrir uma por uma a cada execução do teste: com assertAll
 	@Test
 	void createUser_ShouldInstantiateClass_WhenParametersAreFilled() throws ValidationException {
 		// Arrange
@@ -22,39 +23,53 @@ class UserTest {
 		User user1 = new User(id, name, email, password);
 		
 		// Assert
-		Assertions.assertEquals(0L, user1.getId());
-		Assertions.assertEquals("Igor Borba", user1.getName());
-		Assertions.assertEquals("igor@hotmail.com", user1.getEmail());
-		Assertions.assertEquals("123456", user1.getPassword());
+		Assertions.assertAll(
+				() -> Assertions.assertEquals(0L, user1.getId()),
+				() -> Assertions.assertEquals("Igor Borba", user1.getName()),
+				() -> Assertions.assertEquals("igor@hotmail.com", user1.getEmail()),
+				() -> Assertions.assertEquals("123456", user1.getPassword())
+				);
 	}
 	
+	// ! Como validar vários campos com a mesma exception, mas mudando a mensagem.
 	@Test
 	void nullFields_ShouldThrowsValidationException_WhenParameterAreNull_ThenDisplaysMessage() throws ValidationException {
 		// Arrange
-		Long id = null;
-		String name = null, 
-			   email = null, 
-			   password = null;
+		Long idNull = null,
+			   idFilled = 0L;
+		String nameNull = null, 
+			   emailNull = null, 
+			   passwordNull = null,
+			   nameFilled = "Igor Borba",
+			   emailFilled = "igor@hotmail.com",
+			   passwordFilled = "123465";
+		
 		
 		// Assert
-		Assertions.assertThrows(ValidationException.class, () -> new User(id, name, email, password), 
+		ValidationException nameException = Assertions.assertThrows(ValidationException.class, () -> new User(idNull, nameNull, emailFilled, passwordFilled), 
 				"Review parameters and class's processing because should throw a Exception");
+		ValidationException emailException = Assertions.assertThrows(ValidationException.class, () -> new User(idFilled, nameFilled, emailNull, passwordFilled),
+				"Review parameters and class's processing because should throw a Exception");
+		ValidationException passwordException = Assertions.assertThrows(ValidationException.class, () -> new User(idFilled, nameFilled, emailFilled, passwordNull),
+				"Review parameters and class's processing because should throw a Exception");
+		
+		Assertions.assertEquals("name is required", nameException.getMessage());
+		Assertions.assertEquals("email is required", emailException.getMessage());
+		Assertions.assertEquals("password is required", passwordException.getMessage());
 	}
 	
+	// Como fazer assertAll com assertThrows para validar uma ou várias exceptions
 	@Test
 	void EmptyFields_ShouldThrowsValidationException_WhenParameterAreEmpty_ThenDisplaysMessage() throws ValidationException {
 		// Arrange
 		Long id = null;
-		String name = "s", 
-			   email = "s", 
-			   password = "s";
+		String name = "", 
+			   email = "", 
+			   password = "";
 		
 		// Assert
 		Assertions.assertAll(
-				() -> Assertions.assertThrows(ValidationException.class, () -> new User(id, name, email, password), "Review parameters and class's processing because should throw a Exception)"),
-				() -> Assertions.assertThrows(ValidationException.class, () -> new User(id, name, email, password), "Review parameters and class's processing because should throw a Exception)"),
-				() -> Assertions.assertThrows(ValidationException.class, () -> new User(id, name, email, password), "Review parameters and class's processing because should throw a Exception)")
-		);
-						
+				() -> Assertions.assertThrows(ValidationException.class, () -> new User(id, name, email, password), "Review parameters and class's processing because should throw a Exception)"));
 	}
+	
 }
