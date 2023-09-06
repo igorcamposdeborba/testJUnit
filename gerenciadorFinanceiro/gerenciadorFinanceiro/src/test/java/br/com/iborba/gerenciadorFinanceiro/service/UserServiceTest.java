da055b4876c9bedc4c1a62007644d786c50cc5a5
igorcamposdeborba <igor_campos5@hotmail.com>
2023-09-05 21:21:38 -0300

    Mockito com duas verificações

--- gerenciadorFinanceiro/gerenciadorFinanceiro/src/test/java/br/com/iborba/gerenciadorFinanceiro/service/UserServiceTest.java
+++ gerenciadorFinanceiro/gerenciadorFinanceiro/src/test/java/br/com/iborba/gerenciadorFinanceiro/service/UserServiceTest.java
@@ -50,7 +50,7 @@ public class UserServiceTest {
 		
 		// Treinamento do mock (cadastro de valores que o Mock consegue responder)
 		Mockito.when(userRepository.getUserByEmail("igor@hotmail.com"))
-								   .thenReturn(Optional.of(new User(null, "Igor Borba", "igor@hotmail.com", "123456"))); // comportamento que eu pré-defini
+								   			.thenReturn(Optional.of(new User(null, "Igor Borba", "igor@hotmail.com", "123456"))); // comportamento que eu pré-defini
 		
 		// Act
 		Optional<User> user = userService.getUserByEmail("igor@hotmail.com");
@@ -59,5 +59,28 @@ public class UserServiceTest {
 		Assertions.assertTrue(user.isPresent());
 		Mockito.verify(userRepository).getUserByEmail("igor@hotmail.com"); // validar valor do mock processado
 		Mockito.verify(userRepository, Mockito.times(1)).getUserByEmail("igor@hotmail.com"); // validar o número de vezes que o mock foi chamado (1 vez)
+	}
+	
+	@Test
+	void userValid_ShouldSaveUserSuccessfully_WhenUserServiceMethodsaveUserIsCalled() throws ValidationException {
+		// Arrange
+		UserRepository userRepository = Mockito.mock(UserRepository.class); // Criar objeto da classe UserRepository mockado com Mockito
+		UserService userService = new UserService(userRepository);
+		User user = new User(null, "Andressa Silva", "andressa@hotmail.com", "789456");
+		
+		// Treinamento do mock (cadastro de valores que o Mock consegue responder)
+		Mockito.when(userRepository.getUserByEmail(user.getEmail()))
+		   									.thenReturn(Optional.empty()); // comportamento que eu pré-defini
+		
+		Mockito.when(userRepository.saveUser(user))
+											.thenReturn(user); // comportamento que eu pré-defini
+		
+		// Act
+		User userSaved = userService.saveUser(user);
+		
+		// Assert
+		Assertions.assertNotNull(user);
+		Mockito.verify(userRepository).getUserByEmail("andressa@hotmail.com"); // validar valor do mock processado
+		Mockito.verify(userRepository).saveUser(user);
 	}
 }
